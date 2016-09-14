05a6e0499db5b4f997184681373b31c63d116cb1
Joao Ribeiro <jonnybgod@gmail.com>
2016-09-14 17:50:06 +0100

    fix(package.json): Update to RC7

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import {CloudtasksDirective} from './src/directive';
-import {CloudtasksService} from './src/service';
+import { CloudtasksDirective } from './src/directive';
+import { CloudtasksService } from './src/service';
 
 export * from './src/directive';
 export * from './src/service';
 
 export default {
-  directives: [CloudtasksDirective],
-  providers: [CloudtasksService]
+  directives: [ CloudtasksDirective ],
+  providers: [ CloudtasksService ]
 }
